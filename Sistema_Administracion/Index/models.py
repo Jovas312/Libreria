65304f7ae98016b2703122c420ba8434f75a5c70
Jovas <jovanyjavier23@gmail.com>
2021-09-05 23:34:13 -0500

    Librería 3.2

--- Sistema_Administracion/Index/models.py
+++ Sistema_Administracion/Index/models.py
@@ -17,8 +17,11 @@ class Categorias(models.Model):
 class Libros(models.Model):
     titulo = models.CharField(max_length=100)
     autor = models.ForeignKey(Autores, on_delete=models.SET_NULL, null=True)
+    año = models.IntegerField()
+    editor = models.CharField(max_length=100)
+    idioma = models.CharField(max_length=100)
     descripcion = models.TextField()
-    disponibles = models.IntegerField()
+    disponibles = models.BooleanField()
     precio = models.IntegerField()
     categoria = models.ForeignKey(Categorias, on_delete=models.SET_NULL, null=True)
     imagen = models.ImageField(upload_to='productos', null=True)
@@ -26,6 +29,9 @@ class Libros(models.Model):
     def __str__(self):
         return f"""Titulo {self.titulo}
         Autor: {self.autor}
+        Año: {self.año}
+        Editor: {self.editor}
+        Idioma: {self.idioma}
         Descripción: {self.descripcion}
         Libros disponibles: {self.disponibles}
         Precio: {self.precio}
